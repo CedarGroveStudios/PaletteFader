53883e57da229b472372effba5ba7a75433db199
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-07-04 16:25:41 -0700

    move single color method to unit_converter repo

--- code/cedargrove_palettefader.py
+++ code/cedargrove_palettefader.py
@@ -13,7 +13,7 @@ displayio color palette object (displayio.Palette) that can also be read as a
 color list.
 
 For adjusting a single color value, create a list containing a single color or
-use cedargrove_palettefader.set_single_color_brightness().
+use cedargrove_unit_converter.color.colorfader.color_fader().
 
 * Author(s): JG for Cedar Grove Maker Studios
 
@@ -145,33 +145,3 @@ class PaletteFader:
                     + (norm_palette[i, 1] << 8)
                     + norm_palette[i, 2]
                 )
-
-
-def set_single_color_brightness(self, source_color=None, brightness=1.0, gamma=1.0):
-    """Scale a 24-bit RGB source color value in proportion to the brightness
-    setting (0 to 1.0). Returns an adjusted 24-bit RGB color value or None if
-    the source color is None (transparent). The adjusted color's gamma value is
-    typically from 0.0 to 2.0 with a default of 1.0 for no gamma adjustment.
-
-    :param int source_color: The color value to be adjusted. Default is None.
-    :param float brightness: The brightness value for color value adjustment.
-      Value range is 0.0 to 1.0. Default is 1.0 (maximum brightness).
-    :param float gamma: The gamma value for color value adjustment. Value range
-      is 0.0 to 2.0. Default is 1.0 (no gamma adjustment).
-
-    :return int: The adjusted color value."""
-
-    if source_color is None:
-        return
-
-    # Extract primary colors and scale to brightness
-    r = min(int(brightness * ((source_color & 0xFF0000) >> 16)), 0xFF)
-    g = min(int(brightness * ((source_color & 0x00FF00) >> 8)), 0xFF)
-    b = min(int(brightness * ((source_color & 0x0000FF) >> 0)), 0xFF)
-
-    # Adjust result for gamma
-    r = min(int(round((r**gamma), 0)), 0xFF)
-    g = min(int(round((g**gamma), 0)), 0xFF)
-    b = min(int(round((b**gamma), 0)), 0xFF)
-
-    return (r << 16) + (g << 8) + b
